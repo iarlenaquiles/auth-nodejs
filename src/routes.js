3876f09acffa2fe4a89851ee574d17c790a58cfb
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-03 16:49:34 -0300

    ad routes

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import ForgotPasswordController from './app/controllers/ForgotPasswordController';
+import SessionController from './app/controllers/SessionController';
+import ResetPasswordController from './app/controllers/ResetPasswordController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
+
+routes.post('/forgot', ForgotPasswordController.store);
+
+routes.post('/reset', ResetPasswordController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+routes.delete('/users', UserController.destroy);
+
+export default routes;
