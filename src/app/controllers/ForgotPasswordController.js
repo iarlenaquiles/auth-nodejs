d523097635fa798b6cec129642eb68ef339e9a7d
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-03 16:06:00 -0300

    ajusting async

--- src/app/controllers/ForgotPasswordController.js
+++ src/app/controllers/ForgotPasswordController.js
@@ -8,44 +8,48 @@ class ForgotPasswordController {
   async store(req, res) {
     const { email } = req.body;
 
-    async.waterfall([
-      done => {
-        User.findOne({ email }).exec((err, user) => {
-          if (user) {
-            done(err, user);
-          } else {
-            done('User not found');
-          }
-        });
-      },
-      (user, done) => {
-        crypto.randomBytes(20, (err, buffer) => {
-          const token = buffer.toString('hex');
-          done(err, user, token);
-        });
-      },
-      (user, token, done) => {
-        User.findByIdAndUpdate(
-          { _id: user._id },
-          {
-            reset_password_token: token,
-            reset_password_expires: Date.now() + 86400000,
-          },
-          { upsert: true, new: true }
-        ).exec((err, newUser) => {
-          done(err, token, newUser);
-        });
-      },
-      (token, user, done) => {
-        await Queue.add(RecoveryPasswordMail.key, {
-          user,
-          token
-        });
-
-      },
-    ], (err) => {
-      return res.status(422).json({ message: err });
-    });
+    async.waterfall(
+      [
+        done => {
+          User.findOne({ email }).exec((err, user) => {
+            if (user) {
+              done(err, user);
+            } else {
+              done('User not found');
+            }
+          });
+        },
+        (user, done) => {
+          crypto.randomBytes(20, (err, buffer) => {
+            const token = buffer.toString('hex');
+            done(err, user, token);
+          });
+        },
+        (user, token, done) => {
+          User.findByIdAndUpdate(
+            { _id: user._id },
+            {
+              reset_password_token: token,
+              reset_password_expires: Date.now() + 86400000,
+            },
+            { upsert: true, new: true }
+          ).exec((err, newUser) => {
+            done(err, token, newUser);
+          });
+        },
+        (token, user, done) => {
+          Queue.add(RecoveryPasswordMail.key, {
+            user,
+            token,
+          })
+            .then(response => res.json({ message: 'Check your email box' }))
+            .catch(err => done(err));
+        },
+      ],
+      err => {
+        return res.status(422).json({ message: err });
+      }
+    );
   }
 }
 
